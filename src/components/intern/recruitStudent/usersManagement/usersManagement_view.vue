005f6d3942431cc170d6b893505fd92410458835
Lurkerc <434848237@qq.com>
2017-07-01 11:47:43 +0800

    增加实习生-人员管理

---
+++ src/components/intern/recruitStudent/usersManagement/usersManagement_view.vue
@@ -0,0 +1,126 @@
+<!-- 实习生 - 报名招录 - 人员管理 - 查看（档案查看） -->
+<template>
+  <div>
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="基本情况" name="first">
+        <!-- 基本情况-->
+        <keep-alive>
+          <basic :data-id="operailityData.id" v-if="currentView==0"></basic>
+        </keep-alive>
+      </el-tab-pane>
+      <el-tab-pane label="学历信息" name="second">
+        <keep-alive>
+          <education :data-id="operailityData.id" v-if="currentView==1"></education>
+        </keep-alive>
+      </el-tab-pane>
+      <el-tab-pane label="工作经历" name="third">
+        <!--   工作经历-->
+        <keep-alive>
+          <work-experience :data-id="operailityData.id" v-if="currentView==2"></work-experience>
+        </keep-alive>
+      </el-tab-pane>
+      <el-tab-pane label="专业技术职称晋升情况" name="fourth">
+        <!-- 专业技术职称晋升情况-->
+        <keep-alive>
+          <professional-skill :data-id="operailityData.id" v-if="currentView==3"></professional-skill>
+        </keep-alive>
+      </el-tab-pane>
+      <el-tab-pane label="资格证书取得情况" name="five">
+        <!-- 资格证书取得情况-->
+        <keep-alive>
+          <credentials :data-id="operailityData.id" v-if="currentView==4"></credentials>
+        </keep-alive>
+      </el-tab-pane>
+      <el-tab-pane label="教学经历" name="six">
+        <!--教学经历-->
+        <keep-alive>
+          <teaching-experience :data-id="operailityData.id" v-if="currentView==5"></teaching-experience>
+        </keep-alive>
+      </el-tab-pane>
+      <el-tab-pane label="学习培训情况" name="seven">
+        <!--学习培训情况-->
+        <keep-alive>
+          <study :data-id="operailityData.id" v-if="currentView==6"></study>
+        </keep-alive>
+      </el-tab-pane>
+      <el-tab-pane label="培训考试、考核情况" name="eight">
+        <!--培训考试、考核情况-->
+        <keep-alive>
+          <train :data-id="operailityData.id" v-if="currentView==7"></train>
+        </keep-alive>
+      </el-tab-pane>
+      <el-tab-pane label="获奖情况" name="nine">
+        <!--获奖情况-->
+        <keep-alive>
+          <awards :data-id="operailityData.id" v-if="currentView==8"></awards>
+        </keep-alive>
+      </el-tab-pane>
+      <el-tab-pane label="科研信息" name="ten">
+        <!--科研信息-->
+        <keep-alive>
+          <scientific :data-id="operailityData.id" v-if="currentView==9"></scientific>
+        </keep-alive>
+      </el-tab-pane>
+      <el-tab-pane label="证件复印件" name="eleven">
+        <!--证件复印件-->
+        <keep-alive>
+          <papers :data-id="operailityData.id" v-if="currentView==10"></papers>
+        </keep-alive>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+<script>
+  /*引入查看的子组件*/
+  //  引入--基本情况--组件
+  import basic from '../../../teach/archivesManagement/archivesManagement/archivesManagement_view/archivesManagement_view_basic';
+  //  引入--学历信息--组件
+  import education from '../../../teach/archivesManagement/archivesManagement/archivesManagement_view/archivesManagement_view_education';
+  //  引入--工作经历--组件
+  import workExperience from '../../../teach/archivesManagement/archivesManagement/archivesManagement_view/archivesManagement_view_workExperience';
+  //  引入--专业技术职称晋升情况--组件
+  import professionalSkill from '../../../teach/archivesManagement/archivesManagement/archivesManagement_view/archivesManagement_view_professionalSkill';
+  //  引入--资格证书取得情况--组件
+  import credentials from '../../../teach/archivesManagement/archivesManagement/archivesManagement_view/archivesManagement_view_credentials';
+  //  引入--教学经历--组件
+  import teachingExperience from '../../../teach/archivesManagement/archivesManagement/archivesManagement_view/archivesManagement_view_teachingExperience';
+  //  引入--学习培训情况--组件
+  import study from '../../../teach/archivesManagement/archivesManagement/archivesManagement_view/archivesManagement_view_study';
+  //  引入--培训考试、考核情况--组件
+  import train from '../../../teach/archivesManagement/archivesManagement/archivesManagement_view/archivesManagement_view_train';
+  //  引入--获奖情况--组件
+  import awards from '../../../teach/archivesManagement/archivesManagement/archivesManagement_view/archivesManagement_view_awards';
+  //  引入--科研信息--组件
+  import scientific from '../../../teach/archivesManagement/archivesManagement/archivesManagement_view/archivesManagement_view_scientific';
+  //  引入--证件复印件--组件
+  import papers from '../../../teach/archivesManagement/archivesManagement/archivesManagement_view/archivesManagement_view_papers';
+
+  export default {
+    props: ['operailityData'],
+    data() {
+      return {
+        activeName: 'first',
+        currentView: 0,
+      };
+    },
+    methods: {
+      handleClick(tab, event) {
+        this.currentView = tab["index"];
+      },
+    },
+    components: {
+      basic,
+      education,
+      workExperience,
+      professionalSkill,
+      credentials,
+      teachingExperience,
+      study,
+      train,
+      awards,
+      scientific,
+      papers
+    }
+  };
+
+</script>
