c7605302c1b5ee45013e06ca12012ab89d83ea9a
Lurkerc <434848237@qq.com>
2017-08-23 19:41:14 +0800

    备份原来的课程（状态及审核流程）

---
+++ src/components/teach/teacherResManagement/normCourse/dataTool.1.js
@@ -0,0 +1,82 @@
+import _ from "lodash";
+
+/**
+ * 获取标准课程提交数据
+ * @param {*} course 
+ * @param {*} evaluate 
+ * @param {*} planDtoList
+ * @param status 状态
+ */
+const getNormCourse = function (course, evaluate, planDtoList, status) {
+  let data = _.defaultsDeep({}, course); // 课程基本信息
+  data.evaluate = _.defaultsDeep({}, evaluate); // 教学质量评价对象
+  data.status = status;
+  data.planDtoList = [];
+  planDtoList.map((items, index) => {
+    let item = _.defaultsDeep({}, items);
+    item.no = index + 1; // 第几节课
+    // 课件
+    item.wareDtoListTemp.before.map(beforeWareItem => {
+      item.wareDtoList.push(beforeWareItem)
+    })
+    item.wareDtoListTemp.in_progress.map(in_progressWareItem => {
+      item.wareDtoList.push(in_progressWareItem)
+    })
+    item.wareDtoListTemp.after.map(beforeWareItem => {
+      item.wareDtoList.push(beforeWareItem)
+    })
+    // 作业
+    item.testingDtoList.push(item.testingDtoListTemp.before);
+    item.testingDtoList.push(item.testingDtoListTemp.in_progress);
+    item.testingDtoList.push(item.testingDtoListTemp.after);
+    // 删除无用值
+    delete(item.wareDtoListTemp);
+    delete(item.testingDtoListTemp);
+    data.planDtoList.push(item)
+  })
+  delete(data.createTime);
+  return data
+}
+
+/**
+ * 获取授课提交数据
+ * @param {*} course 
+ * @param {*} evaluate 
+ * @param {*} planDtoList
+ * @param auditStatus 状态
+ */
+const getWebCourse = function (course, evaluate, planDtoList, auditStatus) {
+  let data = _.defaultsDeep({}, course); // 课程基本信息
+  data.evaluate = _.defaultsDeep({}, evaluate); // 教学质量评价对象
+  data.auditStatus = auditStatus;
+  data.planDtoList = [];
+  planDtoList.map((items, index) => {
+    let item = _.defaultsDeep({}, items);
+    item.no = index + 1; // 第几节课
+    // 课件
+    item.wareDtoListTemp.before.map(beforeWareItem => {
+      item.wareDtoList.push(beforeWareItem)
+    })
+    item.wareDtoListTemp.in_progress.map(in_progressWareItem => {
+      item.wareDtoList.push(in_progressWareItem)
+    })
+    item.wareDtoListTemp.after.map(beforeWareItem => {
+      item.wareDtoList.push(beforeWareItem)
+    })
+    // 作业
+    item.testingDtoList.push(item.testingDtoListTemp.before);
+    item.testingDtoList.push(item.testingDtoListTemp.in_progress);
+    item.testingDtoList.push(item.testingDtoListTemp.after);
+    // 删除无用值
+    delete(item.wareDtoListTemp);
+    delete(item.testingDtoListTemp);
+    data.planDtoList.push(item)
+  })
+  delete(data.createTime);
+  return data
+}
+
+export {
+  getNormCourse,
+  getWebCourse,
+}
